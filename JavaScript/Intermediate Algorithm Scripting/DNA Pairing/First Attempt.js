ecfd1be75345e2ec5fa20cd5ec3a5fe5a145dab0
gignu <52784332+gignupg@users.noreply.github.com>
2020-01-10 17:39:20 +0100

    Create First Attempt.js

---
+++ JavaScript/Intermediate Algorithm Scripting/DNA Pairing/First Attempt.js
@@ -0,0 +1,26 @@
+function pairElement(str) {
+    str = str.split("")
+    let newArr = []
+
+    for (let i in str){
+        newArr.push([str[i]])
+        switch(str[i]){
+            case "T":
+                newArr[i].push("A")
+                break
+            case "A":
+                newArr[i].push("T")
+                break
+            case "G":
+                newArr[i].push("C")
+                break
+            case "C":
+                newArr[i].push("G")
+                break    
+        }
+        
+    }
+    return newArr
+  }
+  
+  pairElement("GCG")
