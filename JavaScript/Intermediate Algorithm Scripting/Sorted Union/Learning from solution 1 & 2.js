ea8b616aba2f4e9b25baa022fb4c2f156f52713a
gignu <52784332+gignupg@users.noreply.github.com>
2020-01-12 21:25:57 +0100

    Update and rename Learning from solution 1-3.js to Learning from solution 1 & 2.js

--- JavaScript/Intermediate Algorithm Scripting/Sorted Union/Learning from solution 1 & 2.js
+++ JavaScript/Intermediate Algorithm Scripting/Sorted Union/Learning from solution 1 & 2.js
@@ -13,8 +13,4 @@ function uniteUnique(arr1, arr2, arr3) {
 // let args = [...arguments]
 // That's a really nice way too, especially because there are no arguments required. If I wanted I could delete arr1 arr2 and arr3.
 
-// Solution 3:
-// let args = Array.prototype.slice.call(arguments) or let args = [].slice.call(arguments)
-// Another way to transform something into an array. Kind of long winded but good to know, in case I see it somewhere. 
-// 
-// 
+
