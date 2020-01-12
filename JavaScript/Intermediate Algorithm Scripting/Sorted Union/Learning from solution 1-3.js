a19ab358491dce16015effbb6fb98b63c62a0ec1
gignu <52784332+gignupg@users.noreply.github.com>
2020-01-12 17:41:58 +0100

    Update and rename Learning from solution 1.js to Learning from solution 1-3.js

--- JavaScript/Intermediate Algorithm Scripting/Sorted Union/Learning from solution 1-3.js
+++ JavaScript/Intermediate Algorithm Scripting/Sorted Union/Learning from solution 1-3.js
@@ -7,3 +7,7 @@ function uniteUnique(arr1, arr2, arr3) {
 
     return result;
   }
+
+// Solution 3:
+// args = Array.prototype.slice.call(arguments)
+// Another way to transform something into an array. Kind of long winded but good to know, in case I see it somewhere. 
