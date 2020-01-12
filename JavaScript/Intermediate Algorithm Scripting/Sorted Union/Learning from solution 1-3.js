1658164add215ebf8b93c34929b7aa75366f6b15
gignu <52784332+gignupg@users.noreply.github.com>
2020-01-12 18:40:46 +0100

    Update Learning from solution 1-3.js

--- JavaScript/Intermediate Algorithm Scripting/Sorted Union/Learning from solution 1-3.js
+++ JavaScript/Intermediate Algorithm Scripting/Sorted Union/Learning from solution 1-3.js
@@ -10,7 +10,7 @@ function uniteUnique(arr1, arr2, arr3) {
   }
 
 // Solution 2:
-// let args = args = [...arguments]
+// let args = [...arguments]
 // That's a really nice way too, especially because there are no arguments required. If I wanted I could delete arr1 arr2 and arr3.
 
 // Solution 3:
