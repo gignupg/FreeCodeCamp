66a3012bab52e72f62a85ebc04a6de8af9f321c1
gignu <52784332+gignupg@users.noreply.github.com>
2020-01-14 22:56:18 +0100

    Update Learning from solution 2.js

--- JavaScript/Intermediate Algorithm Scripting/Convert HTML Entities/Learning from solution 2.js
+++ JavaScript/Intermediate Algorithm Scripting/Convert HTML Entities/Learning from solution 2.js
@@ -8,11 +8,14 @@ function convertHTML(str) {
         ">": "&gt;"
     }
 
-    str = str.replace(/[&'"<>]/g, x => converter[x])
-
-    return str
+return str.replace(/[&'"<>]/g, x => converter[x])
 }
   
 console.log(convertHTML("Hamburgers < Pizza < Tacos"))
 
-// Using replace with a replacement function in combination with an object
+// Solution 2
+// Using replace() with a replacement function in combination with an object
+
+// Solution 3
+// return str.split("").map(x => converter[x] || x).join("")
+// Apparently when map tries to use converter[x] and gets undefined, it looks if it can get a value from the other return statement after the "or"
