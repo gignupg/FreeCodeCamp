c3f3cba52abb0bdc3b20a1660ee8bf3ac7fe285b
gignu <gignupg@googlemail.com>
2020-01-30 01:39:18 +0100

    deleting a test comment

--- JavaScript/Intermediate Algorithm Scripting/Sum All Primes/My Solution 1.js
+++ JavaScript/Intermediate Algorithm Scripting/Sum All Primes/My Solution 1.js
@@ -22,4 +22,3 @@ function sumPrimes(num) {
 // To make the calculation easier for the computer change "value" in the second for loop to "Math.sqrt(value)". Like that i
 // will never get bigger than the square root of "value". Here a great/easy explenation:
 // https://stackoverflow.com/questions/5811151/why-do-we-check-up-to-the-square-root-of-a-prime-number-to-determine-if-it-is-pr
-// Another comment
