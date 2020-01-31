97833cf79904f1612729ad33499e84f26d5d6cf0
gignu <gignupg@googlemail.com>
2020-01-31 14:34:46 +0100

    Planning what to do

--- JavaScript/Intermediate Algorithm Scripting/Sum All Primes/Learning from Solution 1.js
+++ JavaScript/Intermediate Algorithm Scripting/Sum All Primes/Learning from Solution 1.js
@@ -1,5 +1,12 @@
 function sumPrimes(num) {
-    return num;
-  }
 
-  sumPrimes(10);
+}
+
+sumPrimes(10);
+
+// Make an array with all numbers from 2 to num
+
+// Make a filter. For each value check if it is in the composite array.
+// If it is not, add it to the prime array and add all multiples of it to the composite array
+
+// Use reduce to sum all numbers in the prime array.
