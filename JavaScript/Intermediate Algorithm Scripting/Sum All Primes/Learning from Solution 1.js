ac65d064c3f5f835de3dff8513c1d30a08932a71
gignu <gignupg@googlemail.com>
2020-01-31 15:09:24 +0100

    Final solution

--- JavaScript/Intermediate Algorithm Scripting/Sum All Primes/Learning from Solution 1.js
+++ JavaScript/Intermediate Algorithm Scripting/Sum All Primes/Learning from Solution 1.js
@@ -1,12 +1,21 @@
 function sumPrimes(num) {
+  let primes = []
+  let composites = []
+
+  for (let i = 2; i <= num; i++) {
+    if (!composites[i]) {
+      primes.push(i)
+      for (let k = i*2; k < num; k += i) {
+        composites[k] = true
+      }
+    }
+  }
+  return primes.reduce((a, b) => a+b)
 
 }
 
-sumPrimes(10);
+console.log(sumPrimes(977))
 
-// Make an array with all numbers from 2 to num
-
-// Make a filter. For each value check if it is in the composite array.
-// If it is not, add it to the prime array and add all multiples of it to the composite array
-
-// Use reduce to sum all numbers in the prime array.
+// The two main ideas:
+// -Bitwise operators (I could replace i*2 with i<<1 as in the solution)
+// -Making a composite array.
