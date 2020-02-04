90371342b00ad6f776a1a5686b1c5e5a91e1debc
gignu <linus.komnick@googlemail.com>
2020-02-04 14:38:42 +0100

    Solving the challenge

--- JavaScript/Intermediate Algorithm Scripting/Drop It/My Solution.js
+++ JavaScript/Intermediate Algorithm Scripting/Drop It/My Solution.js
@@ -1,8 +1,8 @@
 function dropElements(arr, func) {
-    // Drop them elements.
-    return func([...arr]);
+    for (let i = 0; i < arr.length; i++){
+      if (func(arr[i])) return arr.slice(i)
+    }
+    return []
   }
   
-  dropElements([1, 2, 3], function(n) {return n < 3; });
-  
-  // A new comment
+  console.log(dropElements([1, 2, 3, 4], function(n) {return n >= 3;}))
