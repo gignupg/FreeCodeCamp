e309895ea560b01cb70b8ba0230f087e3a95b2d5
gignu <linus.komnick@googlemail.com>
2020-02-08 14:44:27 +0100

    Arguments Optional

--- JavaScript/Intermediate Algorithm Scripting/Arguments Optional/Learning from Solution 1.js
+++ JavaScript/Intermediate Algorithm Scripting/Arguments Optional/Learning from Solution 1.js
@@ -2,25 +2,23 @@ function addTogether() {
     let a = arguments[0]
     let b = arguments[1]
 
-    function myFunc(){
-        console.log(arguments[0])
+    if (typeof a !== "number"){
+        return undefined
     }
 
-    console.log(myFunc())
-
+    if (b !== undefined){
+        if (typeof b !== "number"){
+            return undefined
+        }
+        return a + b  
+    }
+    
     return function(c){
         if (typeof c !== "number"){
             return undefined
         }
         return a + c
     }
-    
-
-    if (typeof a !== "number" || typeof b !== "number"){
-        return undefined
-    }
-    
-    return a + b
-  }
+}
   
-  console.log(addTogether(2)(3))
+console.log(addTogether(2)([3]))
