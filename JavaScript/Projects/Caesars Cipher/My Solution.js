4c466d104de9aafb533fba4f64fcfa8ae2fe2db9
gignu <linus.komnick@googlemail.com>
2020-03-22 23:18:08 +0100

    Next project

--- JavaScript/Projects/Caesars Cipher/My Solution.js
+++ JavaScript/Projects/Caesars Cipher/My Solution.js
@@ -1,6 +1,23 @@
 function rot13(str) {
 
-    return str
+    let origArr = str.split(" ")
+
+    String.prototype.toRot = function(){
+        let num = this.charCodeAt(0)
+        if (num < 65 || num > 90){
+            return String.fromCharCode(num)
+        } else {
+            num += 13
+            if (num > 90){
+            return String.fromCharCode(num - 90 + 64)
+            }
+            else return String.fromCharCode(num)
+        }
+    }
+
+    let newArr = origArr.map(elem => elem.split("").map(item => item.toRot()))
+
+    return newArr.map(elem => elem.join("")).join(" ")
 }
-  
+
 console.log(rot13("SERR PBQR PNZC"))
